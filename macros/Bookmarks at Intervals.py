28492d23d51353ea467456e36a53e68446e3a4ee
Diego Fernández Gosende <dfgosende@gmail.com>
2012-06-01 15:50:20 +0200

    Improve the 'Bookmarks at Intervals' macro dialog box
    Now the number of intervals can be specified directly, instead of as a
    decimal

--- macros/Bookmarks at Intervals.py
+++ macros/Bookmarks at Intervals.py
@@ -1,15 +1,10 @@
-totalframes = avsp.GetVideoFramecount()
-value = avsp.GetTextEntry("Values of 1 or greater will create bookmarks at that interval. \ne.g. a value of 1000 will create a bookmark every 1000 frames. \nValues between 0 and 1 will distribute bookmarks \nat even intervals throughout the video. \ne.g. a value of 0.2 will create 5 evenly spaced bookmarks.", default="1000", title="Bookmark Interval")
-value = float(value)
-if value >= 1:
-    currentframe = 0
-    while currentframe < totalframes:
-        avsp.SetBookmark(currentframe)
-        currentframe += value
-else:
-    currentframe = 0
-    avsp.SetBookmark(currentframe)
-    interval = totalframes * value
-    while currentframe < totalframes:
-        avsp.SetBookmark(currentframe)
-        currentframe += interval
+value = avsp.GetTextEntry(['Choose a frame step or a number of intervals',
+                           'Frame step', 'Number of intervals'],
+                            '', 'Bookmarks at Intervals', 'sep', width=100)
+if any(value):
+    totalframes = avsp.GetVideoFramecount()
+    step = float(value[0]) if value[0] else totalframes / float(value[1])
+    bm = 0
+    while bm < totalframes - 1:
+        avsp.SetBookmark(bm)
+        bm += step
