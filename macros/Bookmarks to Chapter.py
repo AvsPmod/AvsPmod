2b01df6ecfd9533f460bbc996e00b4fb414b22f1
Diego Fernández Gosende <dfgosende@gmail.com>
2012-06-01 15:45:17 +0200

    Fixes in the 'Bookmarks to Chapter' macro
    - Accept Unicode
    - Add a .txt files filter to the save dialog
    - Only refresh the script if an output filename is returned

--- macros/Bookmarks to Chapter.py
+++ macros/Bookmarks to Chapter.py
@@ -1,27 +1,33 @@
-fps = avsp.GetVideoFramerate()
+import codecs
+
+# run in thread
 try:
     bookmarks = avsp.GetBookmarkList(title=True)
 except TypeError:
     bookmarks = avsp.GetBookmarkList()
 bookmarks.sort()
-filename = avsp.GetSaveFilename(title='Save as')
-if filename:
-    text = []
-    chapter = 1
-    for item in bookmarks:
-        if type(item) is int:
-            bookmark = item
-            title = ''
-        else:
-            bookmark, title = item
-        m, s = divmod(bookmark/fps, 60)
-        h, m = divmod(m, 60)
-        timecode = 'CHAPTER%02d=%02d:%02d:%06.3f\n' % (chapter, h ,m, s)
-        if not title:
-            title = 'Chapter %02d' % chapter
-        title = 'CHAPTER%02dNAME=%s\n' % (chapter, title)
-        text += [timecode, title]
-        chapter +=1
-    f = open(filename, 'w')
+filename = avsp.GetSaveFilename(title='Save as', 
+                                filefilter = '|'.join((_('Text files (*.txt)|*.txt'),
+                                                       _('All files (*.*)|*.*'))))
+if not filename:
+    return
+fps = avsp.GetVideoFramerate()
+text = []
+chapter = 1
+for item in bookmarks:
+    if type(item) is int:
+        bookmark = item
+        title = ''
+    else:
+        bookmark, title = item
+    m, s = divmod(bookmark/fps, 60)
+    h, m = divmod(m, 60)
+    timecode = 'CHAPTER%02d=%02d:%02d:%06.3f\n' % (chapter, h ,m, s)
+    if not title:
+        title = 'Chapter %02d' % chapter
+    title = 'CHAPTER%02dNAME=%s\n' % (chapter, title)
+    text += [timecode, title]
+    chapter +=1
+    f = codecs.open(filename, 'w', 'utf-8')
     f.writelines(text)
     f.close()
