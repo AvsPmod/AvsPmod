978d75c4ea4fdcb2ad62f0866332c281f375c3ff
Diego Fernández Gosende <dfgosende@gmail.com>
2012-06-04 00:20:10 +0200

    Bump minimum required Python version to 2.6
    Some of the last commits introduced unintentionally incompatibility
    with Python 2.5.  Chance to move on.
    
    Also complete the build_instructions.

--- AvsP_setup.py
+++ AvsP_setup.py
@@ -7,7 +7,6 @@ appName = "AvsPmod"
 description = "%s - an AviSynth script editor" % appName
 url = ''
 license = 'GNU GPL v2'
-isVC90 = sys.version_info > (2, 6)
 
 MANIFEST_TEMPLATE = """
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
@@ -44,9 +43,7 @@ MANIFEST_TEMPLATE = """
 </assembly>
 """
 
-manifest_extra = {
-    0: '',
-    1: """\
+manifest_extra = """\
   <dependency>
     <dependentAssembly>
       <assemblyIdentity
@@ -58,12 +55,9 @@ manifest_extra = {
       </assemblyIdentity>
     </dependentAssembly>
   </dependency>
-""",}
+"""
 
-lib_extra = {
-    0: [],
-    1: [os.path.join(os.path.dirname(AvsP.wx.__file__), 'gdiplus.dll')],
-    }
+lib_extra = [os.path.join(os.path.dirname(AvsP.wx.__file__), 'gdiplus.dll')]
 
 data_files = [
         ('', [
@@ -73,7 +67,7 @@ data_files = [
             'copying.txt',
             ]
         ),
-        ('lib', lib_extra[isVC90]),
+        ('lib', lib_extra),
         ('src', [
             'run.py',
             'AvsP.py',
@@ -93,26 +87,22 @@ data_files = [
 # Include the Microsoft Visual C runtime DLLs for Python 2.6+
 # v9.0.21022.8, available in the Microsoft Visual C++ 2008 Redistributable Package (x86)
 # <https://www.microsoft.com/en-us/download/details.aspx?id=29>
-if isVC90:
-    crt_dst_dir = 'Microsoft.VC90.CRT'
-    crt_version = 'x86_microsoft.vc90.crt_1fc8b3b9a1e18e3b_9.0.21022.8_none_bcb86ed6ac711f91'
-    crt_files = ('msvcm90.dll', 'msvcp90.dll', 'msvcr90.dll')
-    crt_dirs = (os.path.expandvars(os.path.join('%windir%', 'winsxs', crt_version)), 
-                sys.prefix, os.path.join(sys.prefix, 'DLLs'))
-    crt_paths = []
-    for file in crt_files:
-        for dir in crt_dirs:
-            path = os.path.join(dir, file)
-            if os.path.isfile(path):
-                crt_paths.append(path)
-                break
-        else:
-            crt_paths.append(file)
-#    data_files.append((crt_dst_dir, crt_paths)) # it doesn't seem to work
-    data_files.append(('', crt_paths))
-# Include MSVCR71.dll for Python 2.5
-#else:
-#    data_files.append(('', 'msvcr71.dll'))
+crt_dst_dir = 'Microsoft.VC90.CRT'
+crt_version = 'x86_microsoft.vc90.crt_1fc8b3b9a1e18e3b_9.0.21022.8_none_bcb86ed6ac711f91'
+crt_files = ('msvcm90.dll', 'msvcp90.dll', 'msvcr90.dll')
+crt_dirs = (os.path.expandvars(os.path.join('%windir%', 'winsxs', crt_version)), 
+            sys.prefix, os.path.join(sys.prefix, 'DLLs'))
+crt_paths = []
+for file in crt_files:
+    for dir in crt_dirs:
+        path = os.path.join(dir, file)
+        if os.path.isfile(path):
+            crt_paths.append(path)
+            break
+    else:
+        crt_paths.append(file)
+#data_files.append((crt_dst_dir, crt_paths)) # it doesn't seem to work
+data_files.append(('', crt_paths))
 
 # If a resource file doesn't exist in the current directory, take it from its parent
 for i, (dsr, files) in enumerate(data_files):
@@ -172,15 +162,14 @@ setup(
             "script": "run.py",
             "icon_resources": [(1, "AvsP.ico")],
             "other_resources" : [(24, 1, MANIFEST_TEMPLATE % 
-                dict(prog=appName, extra=manifest_extra[isVC90])
+                dict(prog=appName, extra=manifest_extra)
             )],
         }
     ],
 )
 
 # Write the manifest file for the CRT DLLs
-if isVC90:
-    manifest = '''<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+manifest = '''<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <!-- Copyright (c) Microsoft Corporation.  All rights reserved. -->
 <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
     <noInheritable/>
@@ -193,18 +182,18 @@ if isVC90:
     />
     <file name="msvcr90.dll" /> <file name="msvcp90.dll" /> <file name="msvcm90.dll" />
 </assembly>'''
-    for i, arg in enumerate(sys.argv):
-        if arg.lower() == '-d':
-            dist_dir = sys.argv[i+1].strip('"')
-            break
-        else:
-            dir = arg.lower().partition('--dist-dir=')[2]
-            if dir:
-                dist_dir = dir.strip('"')
-                break
+for i, arg in enumerate(sys.argv):
+    if arg.lower() == '-d':
+        dist_dir = sys.argv[i+1].strip('"')
+        break
     else:
-        dist_dir = 'dist'
-#    f = open(os.path.join(dist_dir, crt_dst_dir, crt_dst_dir + '.manifest'), 'w') # it doesn't seem to work
-    f = open(os.path.join(dist_dir, '', crt_dst_dir + '.manifest'), 'w')
-    f.write(manifest)
-    f.close()
+        dir = arg.lower().partition('--dist-dir=')[2]
+        if dir:
+            dist_dir = dir.strip('"')
+            break
+else:
+    dist_dir = 'dist'
+#f = open(os.path.join(dist_dir, crt_dst_dir, crt_dst_dir + '.manifest'), 'w') # it doesn't seem to work
+f = open(os.path.join(dist_dir, '', crt_dst_dir + '.manifest'), 'w')
+f.write(manifest)
+f.close()
