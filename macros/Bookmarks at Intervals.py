82cf7c5a58c2becd90e8462fe5b64125bc9f43fb
Diego Fernández Gosende <dfgosende@gmail.com>
2012-06-07 03:51:21 +0200

    Fixed performance issues in the 'Bookmarks at Intervals' macro
    Fixes #1 - thanks tophf

--- macros/Bookmarks at Intervals.py
+++ macros/Bookmarks at Intervals.py
@@ -4,7 +4,8 @@ value = avsp.GetTextEntry([_('Choose a frame step or a number of intervals'),
 if any(value):
     totalframes = avsp.GetVideoFramecount()
     step = float(value[0]) if value[0] else totalframes / float(value[1])
-    bm = 0
-    while bm < totalframes - 1:
-        avsp.SetBookmark(bm)
-        bm += step
+    def float_range(start=0, end=10, step=1):
+        while start < end:
+            yield start
+            start += step
+    avsp.SetBookmark(float_range(0, totalframes - 1, step))
