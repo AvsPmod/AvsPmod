5128fbfeaec27fe947df981bc7d5ad93af389209
tophf <tophf@mail.com>
2013-03-08 16:02:43 +0100

    [Bookmarks at Intervals] Add custom range and clear existing bookmarks option
    Closes #18

--- macros/Bookmarks at Intervals.py
+++ macros/Bookmarks at Intervals.py
@@ -1,11 +1,23 @@
-value = avsp.GetTextEntry([_('Choose a frame step or a number of intervals'),
-                           _('Frame step'), _('Number of intervals')],
-                            '', _('Bookmarks at Intervals'), 'sep', width=100)
-if any(value):
-    totalframes = avsp.GetVideoFramecount()
-    step = float(value[0]) if value[0] else totalframes / float(value[1])
-    def float_range(start=0, end=10, step=1):
-        while start < end:
+last_frame = avsp.GetVideoFramecount() - 1
+options = avsp.GetTextEntry(
+    [_('Choose a frame step or a number of intervals'),
+     [_('Frame step'), _('Number of intervals')],
+     [_('Start frame'), _('End frame')], 
+     _('Clear bookmarks in the same range')],
+    ['', [(0, 0), (0, 0)], [(0, 0, last_frame), (last_frame, 0, last_frame)]], 
+    _('Bookmarks at Intervals'),
+    ['sep', ['spin', 'spin'], ['spin', 'spin'], 'check'], width=100)
+if options:
+    step, intervals, start, end, clear = options
+else:
+    return
+if clear:
+    avsp.ClearBookmarks(start, end)
+if intervals:
+    step = float(end - start + 1) / intervals
+if step:
+    def float_range(start, end, step):
+        while start <= end:
             yield start
             start += step
-    avsp.SetBookmark(float_range(0, totalframes - 1, step))
+    avsp.SetBookmark(float_range (start, end, step))
